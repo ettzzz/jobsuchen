d5c43006726cf05af1ce9510c30ceb957ce0b20d
ettzzz <39869906+ettzzz@users.noreply.github.com>
2019-07-25 10:28:19 +0800

    scheduler updated

---
+++ call.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 09:35:47 2019
+
+@author: ert
+"""
+
+from local_var import token, chat_id
+from bot import tellMyBot
+from job_db import jobDataBase
+import time
+
+def main():
+    db = jobDataBase()
+    messager = tellMyBot(token, chat_id)
+    
+    new_jobs = db.fetch('new')
+    print('call: Sending messages to bot...\n')
+    messager.send2me(new_jobs)
+    new_timestamp = int(time.strftime('%y%m%d%H%M',time.localtime(time.time())))
+    db.addTimestamp(new_timestamp)
+    print('call: Done!\n')
+    
+if __name__ == '__main__':
+    main()
