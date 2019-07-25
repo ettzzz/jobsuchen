9b975158ddc766a32a7679ea4b4bbc2311a5f5f5
ettzzz <39869906+ettzzz@users.noreply.github.com>
2019-07-25 14:43:14 +0800

    Revert "scheduler updated"
    This reverts commit d5c43006726cf05af1ce9510c30ceb957ce0b20d.

--- run_job.py
+++ run_job.py
@@ -17,7 +17,7 @@ from local_var import token, chat_id, browser_path
 
 job_cfgs = {
             'global':{
-                    'crawls_per_day':12,
+                    'crawls_per_day':2,
                     'crawl_size':90,
                     'db_name': 'jobs.db',
                     'ptable_name':'pool',
@@ -75,30 +75,37 @@ job_cfgs = {
 
 def main(cfgs):
     crawls_per_day = cfgs['global']['crawls_per_day']
+    messager = tellMyBot(token, chat_id)
+    agent = jobSpider(cfgs)
     db = jobDataBase()
     # future todos: change the structure of cfgs, categories: global, bot, db, spider,
     
     while True:
         counter = 0
         while counter < crawls_per_day: 
-            print('main: crawling sequence {}/{},\n'.format(counter + 1, crawls_per_day))
-            agent = jobSpider(cfgs)
             t_start = time.time()
+            table_name = 'table' + time.strftime('%m%d%H%M',time.localtime(time.time()))
+            print('main: crawling sequence {}/{}, data will be written into {}\n'.format(counter + 1, crawls_per_day, table_name))
+            
+            db.addTable(table_name)
             current_pool = db.poolShow()
             captured = agent.scheduler(current_pool)
-            db.insert(captured)
+            db.insert(table_name, captured)
+            
+            print('main: Sending messages to bot...\n')
+            messager.send2me(db.fetch(table_name))
             
             t_crawl = int(time.time() - t_start)
             t_sleep = 86400/crawls_per_day - t_crawl + random.randint(-600, 600)
             t_sleep = t_sleep if t_sleep > 0 else 0
             print('main: Ok, Ich werde {} Stunde schalfen\n'.format(round(t_sleep/3600, 2)))
-            time.sleep(t_sleep)
+#            time.sleep(t_sleep)
             time.sleep(3) # for test
             counter += 1
-        break
+            
         if db.poolCheck() > 2000:
-            messager = tellMyBot(token, chat_id)
             messager.poolAlert(db.poolCheck())
+        db.dropEmpty()
         gc.collect()
         
 if __name__ == '__main__':
